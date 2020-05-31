fb7941a4a7f07f97770ddf628adce377dc62a68f
Jhosep Guadalupe <jhosepgc14@gmail.com>
2020-05-31 17:59:13 -0500

    Updates

---
+++ precache-manifest.5e6a43636646610e6a71b6c432ac436d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c57b3664ad20bfafbc0d823faee5973",
+    "url": "/carrito-codiGo/index.html"
+  },
+  {
+    "revision": "39448bf8736ad215f003",
+    "url": "/carrito-codiGo/static/css/2.e94183ad.chunk.css"
+  },
+  {
+    "revision": "40d3f9940cb908c1c143",
+    "url": "/carrito-codiGo/static/css/main.580dc5dc.chunk.css"
+  },
+  {
+    "revision": "39448bf8736ad215f003",
+    "url": "/carrito-codiGo/static/js/2.15ac9990.chunk.js"
+  },
+  {
+    "revision": "9be48472b35189e9c6b1ed6407464107",
+    "url": "/carrito-codiGo/static/js/2.15ac9990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d3f9940cb908c1c143",
+    "url": "/carrito-codiGo/static/js/main.78f954b2.chunk.js"
+  },
+  {
+    "revision": "86c9d2051fc2dd4094a0",
+    "url": "/carrito-codiGo/static/js/runtime-main.18ff1a01.js"
+  },
+  {
+    "revision": "ff7d24582c4f37e30dad9c6611953fb0",
+    "url": "/carrito-codiGo/static/media/arrow.ff7d2458.svg"
+  },
+  {
+    "revision": "c95d32a10dcfd8e127144191e448ea26",
+    "url": "/carrito-codiGo/static/media/car.c95d32a1.svg"
+  },
+  {
+    "revision": "8690c88b272d2d13e6575e2930d327b0",
+    "url": "/carrito-codiGo/static/media/check1.8690c88b.jpg"
+  },
+  {
+    "revision": "9c28f52a56cc6574c449aefa625f3f52",
+    "url": "/carrito-codiGo/static/media/delete.9c28f52a.svg"
+  },
+  {
+    "revision": "23c43d0bbef4bdfa5a614cb8739656ff",
+    "url": "/carrito-codiGo/static/media/item.23c43d0b.png"
+  },
+  {
+    "revision": "898b54e82d0bb5d4cbd3812cf6b2191d",
+    "url": "/carrito-codiGo/static/media/niubiz.898b54e8.png"
+  },
+  {
+    "revision": "1be3b700bbb11934aceaffda7d46891d",
+    "url": "/carrito-codiGo/static/media/user.1be3b700.svg"
+  },
+  {
+    "revision": "06aa1179b6e9317abc9dcec14ac94881",
+    "url": "/carrito-codiGo/static/media/whatsapp.06aa1179.svg"
+  }
+]);
